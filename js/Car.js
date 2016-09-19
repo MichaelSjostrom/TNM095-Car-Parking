b3f1bcbe3113b8463bfd2ee344741e412aea489a
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-09-19 13:47:29 +0200

    Update comments with constrains

--- js/Car.js
+++ js/Car.js
@@ -31,21 +31,24 @@ class Car {
 
     this.xPos++;
 
+    // We need different constrains here and the pos needs to rely on the tiles
     if (this.xPos > this.width - 5) {
       this.xPos = 0;
     }
 
     if (this.requestId && this.counter < 500) {
-      // clear
+      // Clear previous frame
       this.context.clearRect(0, 0, this.width, this.height);
 
-      // render new car pos
+      // Render car with new pos
       this.renderCar(this.xPos, this.yPos);
 
+      // Do again
       var self = this;
       window.requestAnimFrame(function() { self.animateCar(); });
 
     } else {
+      // Cancel animation
       cancelAnimationFrame(this.requestId);
       this.requestId = undefined;
     }
