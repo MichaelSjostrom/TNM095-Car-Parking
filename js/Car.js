80f1146b0808c00a9fa74af90c24173d628d167d
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-09-21 09:22:27 +0200

    Car can go fwd, turning is weird

--- js/Car.js
+++ js/Car.js
@@ -4,7 +4,7 @@ class Car {
     this.width = canvas.getAttribute('width');
     this.height = canvas.getAttribute('height');
     this.context = canvas.getContext('2d');
-    this.xPos = this.yPos = 10;
+    this.xPos = this.yPos = 0;
     this.requestId = 'hej';
     this.context.fillStyle = 'blue';
     this.counter = 0;
@@ -31,26 +31,40 @@ class Car {
 
     this.xPos++;
 
+    var self = this;
+    var otherReq = false;
     // We need different constrains here and the pos needs to rely on the tiles
-    if (this.xPos > this.width - 5) {
-      this.xPos = 0;
+    if (self.counter == 100) {
+      this.xPos--;
+      var xPos = self.xPos;
+      var yPos = self.yPos;
+      console.log('turning');
+      otherReq = true;
+      window.requestAnimFrame(function() {
+        // Clear previous frame
+        self.context.clearRect(0, 0, self.width, self.height);
+        self.context.translate(self.width / 2, self.height / 2);
+        self.context.rotate(90 * Math.PI / 180);
+        self.renderCar(self.xPos, self.yPos);
+        self.context.translate((-1) * self.width / 2, (-1) * self.height / 2);
+        self.animateCar();
+      });
     }
 
-    if (this.requestId && this.counter < 500) {
+    if (self.counter < 200 && !otherReq) {
+      console.log('fwd');
       // Clear previous frame
       this.context.clearRect(0, 0, this.width, this.height);
-
-      // Render car with new pos
-      this.renderCar(this.xPos, this.yPos);
-
+      self.renderCar(self.xPos, self.yPos);
       // Do again
-      var self = this;
       window.requestAnimFrame(function() { self.animateCar(); });
-
-    } else {
-      // Cancel animation
-      cancelAnimationFrame(this.requestId);
-      this.requestId = undefined;
     }
+  }
+
+  turnRight() {
+    console.log('turning');
+    this.context.translate(this.width / 2, this.height / 2);
+    this.context.rotate(90 * Math.PI / 180);
+    this.context.translate(this.xPos, this.yPos);
   }
 }
