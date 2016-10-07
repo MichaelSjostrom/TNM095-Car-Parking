98ec982710cc7d35eb866e55e336b3af1fa7f2c2
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-10-07 09:05:29 +0200

    Set timer to variable of car

--- js/main.js
+++ js/main.js
@@ -5,8 +5,6 @@ window.requestAnimFrame = (function(callBack) {
       };
 })();
 
-var timer, timer2;
-
 var tileSize = 24;
 var body = document.getElementsByTagName('body')[0];
 var canvasContainer = document.getElementsByClassName('container')[0]
@@ -20,6 +18,8 @@ var mousePos;
 var mapRenderer;
 var astar;
 
+var sec = 0, sec2 = 0;
+
 var car, newCar, carCanvas;
 
 function addCar() {
@@ -31,9 +31,9 @@ function addCar() {
     state.addCar(newCar);
     var tile = parkingLot.getTile(0, 22);
     var path = state.updateCar(newCar, tile, false);
-    var sec2 = 0;
+
     newCar.startAnimation(path, state.getCars, callback);
-    timer2 = setInterval(function () {
+    newCar.timer = setInterval(function () {
         document.getElementById("seconds2").innerHTML = pad(++sec2 % 60);
         document.getElementById("minutes2").innerHTML = pad(parseInt(sec2 / 60, 10));
     }, 1000);
@@ -63,6 +63,11 @@ function run() {
 	car = new Car(carCanvas, 'blue', 'down');
   car.isGoing = 'down';
 
+  car.timer = setInterval(function () {
+      document.getElementById("seconds").innerHTML = pad(++sec % 60);
+      document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+  }, 1000);
+
   var mapCanvas = document.getElementsByTagName("canvas")[0];
   mapRenderer = new MapRenderer(mapCanvas, parkingLot.getMap);
   mapRenderer.draw();
@@ -70,36 +75,20 @@ function run() {
   state = new State(parkingLot);
   state.addCar(car);
 
-  var path = [];
-  var tempX = car.getX;
-  var tempY = car.getY;
-
-  while(!car.isParked){
-
-    var tempTile = getDirection(car);
-    var temp = state.updateCar(car, tempTile, true);
-
-    // Updating car position after calculations have been made.
-    car.xPos = tempTile.getX*24;
-    car.yPos = tempTile.getY*24;
-
-    for(var i = 0; i < temp.length; i++){
-      path.push(temp[i]);
-    }
-  }
-  car.xPos = tempX;
-  car.yPos = tempY;
-
-  car.startAnimation(path, state.getCars, callback);
+  animateCar(car);
 }
 run();
 
 function callback(car) {
-  if (car.color == 'blue' && car.isParked) {
-    clearInterval(timer);
-  } else if(car.isParked) {
-    clearInterval(timer2);
-  } else {
+
+  clearInterval(car.timer);
+
+  if(!car.isParked) {
+    animateCar(car);
+  }
+}
+
+function animateCar(car) {
     var path = [];
     var tempX = car.getX;
     var tempY = car.getY;
@@ -121,7 +110,6 @@ function callback(car) {
     car.yPos = tempY;
 
     car.startAnimation(path, state.getCars, callback);
-  }
 }
 
 function getDirection(car) {
@@ -204,13 +192,9 @@ window.onkeyup = function(e) {
   }
 }
 
-var sec = 0;
 
 function pad(val) {
     return val > 9 ? val : "0" + val;
 }
-timer = setInterval(function () {
-    document.getElementById("seconds").innerHTML = pad(++sec % 60);
-    document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
-}, 1000);
+
 
